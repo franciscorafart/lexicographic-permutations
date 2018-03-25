70b4be0f40fb54043efb96655f6ff4fdd4da4218
Francisco Rafart <francisco.rafart@gmail.com>
2018-03-25 13:44:38 -0400

    progress

--- euler24.js
+++ euler24.js
@@ -1,5 +1,30 @@
+let array = [
+  [1,2],
+  [2,1]
+]
+
+let perm123 = permutation(array, 3)
+
+let perm1234 = permutation(perm123,4)
+console.log(perm1234)
 //function that takes array and permutes
 
-function permutation(arr){
-  
+
+
+function permutation(arr, element){
+  let res = []
+  for (let i = 0; i<arr.length; i++){
+    for(let j = 0; j<= arr[i].length; j++){
+        let arrLocal = Array.from(arr[i]);
+        arrLocal.splice(j,0,element);
+        res.push(arrLocal);
+    }
+  }
+  return res;
 }
+
+// let array1 = [1,2,3]
+// let array2 = Array.from(array1)
+// array2.push(4)
+// console.log(array1)
+// console.log(array2)
