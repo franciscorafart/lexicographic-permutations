f453b1292c5ce741c23767f9f1b02a49a3be113e
Francisco Rafart <francisco.rafart@gmail.com>
2018-03-26 07:15:14 -0400

    solution

--- euler24.js
+++ euler24.js
@@ -1,20 +1,24 @@
-let array = [
-  [1,2],
-  [2,1]
-]
 
-let perm123 = permutation(array, 3)
+let array = [0,1,2,3,4,5,6,7,8,9]
 
-let perm1234 = permutation(perm123,4)
-console.log(perm1234)
-//function that takes array and permutes
+console.log(permsToOrderedString(allPerms(array))[999999])
 
+//returns all the permutations from elements in an array
+function allPerms(array){
+  let initial = [[array[0]]]
 
+  for (let index = 1; index< array.length; index++){
+    let newArray = permutation(initial, array[index]);
+    initial = newArray;
+  }
+  return initial
+}
 
+//returns an array of arrays with 'element' inserted in all indexes
 function permutation(arr, element){
   let res = []
   for (let i = 0; i<arr.length; i++){
-    for(let j = 0; j<= arr[i].length; j++){
+    for(let j = arr[i].length; j>= 0; j--){
         let arrLocal = Array.from(arr[i]);
         arrLocal.splice(j,0,element);
         res.push(arrLocal);
@@ -23,8 +27,17 @@ function permutation(arr, element){
   return res;
 }
 
-// let array1 = [1,2,3]
-// let array2 = Array.from(array1)
-// array2.push(4)
-// console.log(array1)
-// console.log(array2)
+//function to order the arrays
+function permsToOrderedString(arrayOfArrays){
+  res = [];
+    for (let i = 0; i< arrayOfArrays.length; i++){
+      let s = '';
+      for (let j=0; j<arrayOfArrays[i].length; j++){
+        s += String(arrayOfArrays[i][j]);
+      }
+      res.push(s);
+    }
+    res.sort();
+
+  return res
+}
